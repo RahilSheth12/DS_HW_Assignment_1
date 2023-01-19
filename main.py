d2c69fca247964892d5c73cf66412929f2c0ab5d
Rahil Sheth <99558050+RahilSheth12@users.noreply.github.com>
2023-01-18 20:13:15 -0600

    1-4 are done
    5 has the answer to it on the bottom commented out, but I couldn't figure it out on my own.

--- main.py
+++ main.py
@@ -81,13 +81,111 @@ def numOfWords(x: str):
     counter = 0
     if "\n" in x:
         print('yes')
+        oneLine = x.splitlines()
+        print(oneLine)
+        finalStr = ""
+        for i in oneLine:
+            if oneLine.index(i) == 0:
+                finalStr += i
+            else:
+                finalStr += " " + i
 
-    print(x.splitlines())
-    splitedList = x.split(" ")
-    print(splitedList)
+        print(finalStr)
+        splitedList = finalStr.split(" ")
+        return len(splitedList)
+    else:
+        finalList = x.split(" ")
+        return len(finalList)
 
-    return len(splitedList) + counter
+# print(numOfWords("""Starting the Fall of 2021, the Academic Resource Center is
+# moving to in person tutoring for most subject areas. There are some online
+# tutoring sessions available for some subjects."""))
+# print(numOfWords("Academic Calendar\nAcademic Programs- Graduate\nAcademic Programs- Undergraduate"))
 
 
-print(numOfWords("""-How much money do you have?
--50 dollars."""))
+# 5. ASCII Art
+# def ASCIIArt(x: str):
+#     topSide = len(x) * 2 - 2
+#     leftSide = len(x) - 1
+#     eachLine = ""
+#     fullDrawing = ""
+#     for i in range(0, leftSide * 2 + 1):
+#         for j in range(0, topSide * 2 + 1):
+#             eachLine += "."
+#         # print(eachLine)
+#         fullDrawing += eachLine + "\n"
+#         eachLine = ""
+#
+#     listedDrawing = fullDrawing.split("\n")
+#     listedDrawing.pop()
+#
+#     for i in listedDrawing:
+#         tempLineCounter = 0
+#         tempVar = ""
+#         mid = len(i) // 2
+#         for j in range(0, len(listedDrawing)):
+#             if tempLineCounter == 0:
+#                 tempVar = x[-1]
+#                 result_2 = i[:mid] + tempVar + i[mid + 1:]
+#             if tempLineCounter == 1:
+#                 tempVar = x[-1]
+#         # w = i.index(len(i) - topSide)
+#         print(result_2)
+#         # tempList = [i]
+#         # print(tempList)
+#         # for j in range(0, leftSide * 2):
+#
+#     return listedDrawing
+
+
+# print(ASCIIArt("XY"))
+
+# listedDrawing = []
+# for k in fullDrawing:
+#     # if k == topSide + 1:
+#     #     fullDrawing.replace(k, "*")
+#     if k == " "
+#         listedDrawing +=
+
+
+# ---ANSWER---
+
+
+# chars = 'XY'
+# length = len(chars)
+# string = ''
+# dots = (length * 2 - 1) * 2 - 1
+# for i in range(length):
+#     string1 = ''
+#     string += chars[i]
+#     length1 = len(string)
+#     for j in range(0, length1):
+#         if j % 2 != 0:
+#             string1 += chars[length - 1 - j].center(3, '.')
+#         else:
+#             string1 += chars[length - 1 - j]
+#     for k in range(i - 1, -1, -1):
+#         if k % 2 != 0:
+#             string1 += chars[length - 1 - k].center(3, '.')
+#         else:
+#             string1 += chars[length - 1 - k]
+#     string1 = string1.center(dots, '.')
+#     print(string1)
+#
+# string = ''
+# for i in range(length - 1):
+#     string1 = ''
+#     string += chars[i]
+#     length1 = len(string)
+#     for j in range(length - 1 - i):
+#         if j % 2 != 0:
+#             string1 += chars[length - 1 - j]
+#         else:
+#             string1 += chars[length - 1 - j].center(3, '.')
+#     for k in range(i + 2, length):
+#         if k % 2 != 0:
+#             string1 += chars[k].center(3, '.')
+#         else:
+#             string1 += chars[k]
+#     string1 = string1.center(dots, '.')
+#     print(string1)
